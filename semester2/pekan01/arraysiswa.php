521895ed7c8c5bdb7c9924b7168cb6e91d08b99d
alwiy <92706523+alwiy@users.noreply.github.com>
2022-02-25 11:22:46 +0700

    Add files via upload

---
+++ semester2/pekan01/arraysiswa.php
@@ -0,0 +1,50 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<link rel="stylesheet" href="array.css">
+
+<div class="container">
+    <div class="row col-md-8 col-md-offset-2 custyle">
+<?php
+ $ns1 = ['id'=>1,'nim'=>'01101','uts'=>80,'uas'=>84,'tugas'=>78];
+ $ns2 = ['id'=>2,'nim'=>'01121','uts'=>70,'uas'=>50,'tugas'=>68];
+ $ns3 = ['id'=>3,'nim'=>'01130','uts'=>60,'uas'=>86,'tugas'=>70];
+ $ns4 = ['id'=>4,'nim'=>'01134','uts'=>90,'uas'=>91,'tugas'=>82];
+
+ $ar_nilai = [$ns1, $ns2 , $ns3, $ns4];
+
+?>
+<h3>Daftar Nilai Mahasiswa</h3>
+    <table class="table table-striped custab">
+    <thead>
+        <tr>
+            <th>nomor</th>
+            <th>NIM</th>
+            <th>UTS</th>
+            <th>UAS</th>
+            <th>Tugas</th>
+            <th>Nilai Akhir</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+    $nomor = 1;
+    foreach($ar_nilai as $ns){
+        echo '<tr><td>'.$nomor.'</td>';
+        echo '<td>'.$ns['nim'].'</td>';
+        echo '<td>'.$ns['uts'].'</td>';
+        echo '<td>'.$ns['uas'].'</td>';
+        echo '<td>'.$ns['tugas'].'</td>';
+    $nilai_akhir = ($ns['uts'] + $ns['uas']+$ns['tugas'])/3;
+        echo '<td>'.number_format($nilai_akhir,2,',','.').'</td>';
+        echo '<tr/>';
+    $nomor++;
+    
+}
+?>
+
+    </tbody>        
+    </table>
+    </div>
+</div>
